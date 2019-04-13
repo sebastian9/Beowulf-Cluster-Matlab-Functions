a351740f2c4cf8dbb420c0eb224745ba1e42203e
Sebastian Lopez <sebastianls0228@gmail.com>
2019-04-12 21:42:21 -0500

    Create BeowulfClient.m

---
+++ BeowulfClient.m
@@ -0,0 +1,19 @@
+function [] = BeowulfClient(@fun)
+    % Registrarse en Machines
+    
+    % Revisar Tasks hasta encontrar una task en waiting con el IP de la
+    % máquina
+    
+    % Actualizar status de Machines a working
+    
+    % Actualizar status de Task a working
+    
+    % Correr la función con los inputs de la task
+    
+    % Actualizar el task status a done y output al output de la función
+    
+    % Actualizar el Machine status a waiting
+    
+    % Regresar a revisar tasks hasta encontrar una task....
+    
+end
