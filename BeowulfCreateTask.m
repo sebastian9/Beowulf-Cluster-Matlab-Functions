0fb52262c83fa6a429e088ffb87ba6641fa7e09d
Sebastian Lopez <sebastianls0228@gmail.com>
2019-04-13 21:16:06 -0500

    Update Creat and Update Task function

--- BeowulfCreateTask.m
+++ BeowulfCreateTask.m
@@ -1,5 +1,5 @@
-function[] = BeowulfCreateTask(status,input,output)
-    data = ['utf8=%E2%9C%93&task%5Bstatus%5D=',num2str(status,'%i'),'&task%5Binput%5D=',input,'&task%5Boutput%5D=',output,'&commit=Create+Task'];   
+function[] = BeowulfCreateTask(status,IP,input,output)
+    data = ['utf8=%E2%9C%93&task%5Bstatus%5D=',status,'&task%5Bmachine%5D=',IP,'&task%5Binput%5D=',input,'&task%5Boutput%5D=',output,'&commit=Create+Task'];   
     try
            webwrite('https://beowulf-rocket.herokuapp.com/tasks/',data)  
        catch ME
