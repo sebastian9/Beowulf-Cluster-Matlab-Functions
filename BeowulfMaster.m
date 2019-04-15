8779c759a6b0f98c07c16706eb3a8012c88c6cbf
chemafdz <48068179+chemafdz@users.noreply.github.com>
2019-04-15 10:41:53 -0500

    Update BeowulfMaster.m

--- BeowulfMaster.m
+++ BeowulfMaster.m
@@ -13,8 +13,8 @@ function [outputs,invalid_inputs] = BeowulfMaster(inputs,timemax)
         Machines = BeowulfReadMachines();
         i=1;
         while i <= height(Machines)
-            if strcmp(cell2mat(Machines.status(i)),'Waiting')
-                Client = cell2mat(Machines.ip(i));
+            if strcmp(Machines.status(i),'Waiting')
+                Client = Machines.ip(i);
                 
     % Definir Inputs para la tarea a realizar desde el vector de entrada 
       
@@ -38,50 +38,50 @@ function [outputs,invalid_inputs] = BeowulfMaster(inputs,timemax)
         i=1;
         
         while i <= height(Tasks)
-            if strcmp(cell2mat(Tasks.status(i)),'Waiting')
-                Date = cell2mat(Tasks.updated_at(i));
+            if strcmp(Tasks.status(i),'Waiting')
+                Date = Tasks.updated_at(i);
                 t = datetime('now')-datetime(Date,'InputFormat','dd-MMM-yyyy HH:mm:ss');
                 time = strsplit(char(t),':');
                 timemin = str2num(time{1})*60+str2num(time{2});
                 if timemin >= 1
-                    BeowulfUpdateTask(cell2mat(Tasks.id(i)),'Computer not responding',cell2mat(Tasks.ip(i)),cell2mat(Tasks.input(i)),cell2mat(Tasks.output(i)));
-                    inputs{end+1} = cell2mat(Tasks.input(i));
+                    BeowulfUpdateTask(Tasks.id(i),'Computer not responding',Tasks.ip(i),Tasks.input(i),Tasks.output(i));
+                    inputs{end+1} = Tasks.input(i);
                     machines = BeowulfReadMachines();
                     for j = 1:height(machines)
-                           if strcmp(cell2mat(machines.ip(j)),Tasks.ip(i))
-                                machine_ID = cell2mat(machines.id(j));
-                                machine_status = cell2mat(machines.status(j));
+                           if strcmp(machines.ip(j),Tasks.ip(i))
+                                machine_ID = machines.id(j);
+                                machine_status = machines.status(j);
                            end
                     end
                     if ~strcmp(machine_status,'Disconnected')
-                        BeowulfUpdateMachine(machine_ID,'Disconnected',cell2mat(Tasks.ip(i)))
+                        BeowulfUpdateMachine(machine_ID,'Disconnected',Tasks.ip(i))
                     end
                 end
             end
     % En caso de que una tarea tome un tiempo excesivo (TIME*2) invalidar
     % el ID y la Máquina y reasignar; si pasa más de una vez, mandar el 
     % INPUT a un Blacklist (INVALID_INPUTS)
-            if strcmp(cell2mat(Tasks.status(i)),'Working')
-                Date = cell2mat(Tasks.updated_at(i));
+            if strcmp(Tasks.status(i),'Working')
+                Date = Tasks.updated_at(i);
                 t = datetime('now')-datetime(Date,'InputFormat','dd-MMM-yyyy HH:mm:ss');
                 time = strsplit(char(t),':');
                 timemin = str2num(time{1})*60+str2num(time{2});
                 if timemin > 2*timemax
-                    BeowulfUpdateTask(cell2mat(Tasks.id(i)),'Failed',cell2mat(Tasks.input(i)),cell2mat(Tasks.output(i)));
-                    if sum(ismember(warned_inputs,cell2mat(Tasks.input(i))))==0
-                        warned_inputs{end+1} = cell2mat(Tasks.input(i));
-                        inputs{end+1} = cell2mat(Tasks.input(i));
+                    BeowulfUpdateTask(Tasks.id(i),'Failed',Tasks.input(i),Tasks.output(i));
+                    if sum(ismember(warned_inputs,Tasks.input(i)))==0
+                        warned_inputs{end+1} = Tasks.input(i);
+                        inputs{end+1} = Tasks.input(i);
                     end
-                    if sum(ismember(warned_inputs,cell2mat(Tasks.input(i))))>0
-                        invalid_inputs{end+1} = cell2mat(Tasks.input(i));
+                    if sum(ismember(warned_inputs,Tasks.input(i)))>0
+                        invalid_inputs{end+1} = Tasks.input(i);
                     end
                 end
             end
     % Revisar tasks en Done que no haya recopilado y recopilar su output
-            if strcmp(cell2mat(Tasks.status(i)),'Done')
-                if sum(ismember(finished_inputs,cell2mat(Tasks.input(i))))==0
-                    finished_inputs{end+1} = cell2mat(Tasks.input(i));
-                    finished_outputs{end+1} = cell2mat(Tasks.output(i));
+            if strcmp(Tasks.status(i),'Done')
+                if sum(ismember(finished_inputs,Tasks.input(i)))==0
+                    finished_inputs{end+1} = Tasks.input(i);
+                    finished_outputs{end+1} = Tasks.output(i);
                 end
             end
             i=i+1;
